44312dbda433f1e5d810debd1e29426ab13b07f0
Sam Fan <samisfan@msn.com>
2021-02-11 11:39:54 -0700

    revised connection to include mongodb

--- server.js
+++ server.js
@@ -8,18 +8,17 @@ const PORT = 3000;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
